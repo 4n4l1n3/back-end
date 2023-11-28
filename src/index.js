07aee0abe1d1a8dc651ad78e0f7f64f972bd7c2f
4n4l1n3 <132668983+4n4l1n3@users.noreply.github.com>
2023-11-28 15:15:25 +0000

    Adicionando funções

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import express from "express";      // Requisição do pacote do express
 import dotenv from "dotenv";
+import express from "express";  
 import roteadorAdministrador from "./routes/administrador";
 import roteadorDocumento from "./routes/documento";
 
